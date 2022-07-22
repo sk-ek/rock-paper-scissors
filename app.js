773ad65485698ca4735345adf5f1b1fa9a05e9c7
Vu Dinh Thai Hung <skek.git@gmail.com>
2022-07-22 20:29:48 +0700

    remove unnecessary lines in function getComputerChoice

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
-function getComputerChoice() {
-  randomNumber = Math.floor(Math.random() * 3 + 1);
-  switch (randomNumber) {
-    case 1:
-      computerChoice = "rock";
-      break;
-    case 2:
-      computerChoice = "paper";
-      break;
-    case 3:
-      computerChoice = "scissors";
-      break;
-  }
+const choiceList = ["rock", "paper", "scissors"];
 
-  return computerChoice;
+function getComputerChoice() {
+  return choiceList[Math.floor(Math.random() * choiceList.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
+  if (choiceList.includes(playerSelection)) {
+  }
   playerSelection = playerSelection.toLowerCase();
   switch (true) {
     case playerSelection === computerSelection:
