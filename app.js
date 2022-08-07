c059a912df7f25d7bd118d57f3d2248ca52128b3
Vu Dinh Thai Hung <skek.git@gmail.com>
2022-08-07 16:42:51 +0700

    change the player input method from answer the prompt the clicking the options in the html

--- app.js
+++ app.js
@@ -11,50 +11,40 @@ function playRound(playerSelection, computerSelection) {
   // switch case to check who won based on the playerSelection and the computerSelection
   switch (true) {
     case playerSelection === computerSelection:
-      return `you tied: player: ${playerSelection} | computer: ${computerSelection}`;
+      console.log(
+        `you tied: player: ${playerSelection} | computer: ${computerSelection}`
+      );
       break;
     case playerSelection === "rock" && computerSelection === "scissors":
-      return `you won: player: ${playerSelection} | computer: ${computerSelection}`;
+      console.log(
+        `you won: player: ${playerSelection} | computer: ${computerSelection}`
+      );
       break;
     case playerSelection === "paper" && computerSelection === "rock":
-      return `you won: player: ${playerSelection} | computer: ${computerSelection}`;
+      console.log(
+        `you won: player: ${playerSelection} | computer: ${computerSelection}`
+      );
       break;
     case playerSelection === "scissors" && computerSelection === "paper":
-      return `you won: player: ${playerSelection} | computer: ${computerSelection}`;
+      console.log(
+        `you won: player: ${playerSelection} | computer: ${computerSelection}`
+      );
       break;
     // if all the above doesn't get executed then the player lost so we just need to default it and not make another 3 cases
     default:
-      return `you lost: player: ${playerSelection} | computer: ${computerSelection}`;
+      console.log(
+        `you lost: player: ${playerSelection} | computer: ${computerSelection}`
+      );
       break;
   }
 }
 
-function getPlayerChoice() {
+function playGame() {
   box.forEach((boxItem) =>
-    boxItem.addEventListener("click", (e) => console.log(e.target.id))
+    boxItem.addEventListener("click", (e) =>
+      playRound(e.target.id, getComputerChoice())
+    )
   );
 }
 
-// function to play rock paper scissors 5 times
-function game() {
-  for (let i = 0; i < 5; i++) {
-    console.log(playRound(getPlayerChoice(), getComputerChoice()));
-  }
-}
-
-function promptChoice() {
-  let playerSelection = "";
-  /*
-  while the playerSelection
-  (the string that the player input into the prompt 
-  (although the first value of playerSelection will be undefined because we did let playerSelection ))
-  is not in choiceList, it will prompt the prompt forever until it's in choiceList 
-  
-  */
-  while (!choiceList.includes(playerSelection)) {
-    playerSelection = prompt("choose one (rock, paper, scissors) : ");
-    playerSelection =
-      typeof playerSelection === "string" ? playerSelection.toLowerCase() : "";
-  }
-  return playerSelection;
-}
+playGame();
